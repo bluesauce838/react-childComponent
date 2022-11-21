038f33736981e812318579eb74c750b9069487b2
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-11-21 18:32:16 +0530

    1 file modified

--- src/components/Callapi.jsx
+++ src/components/Callapi.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+
+
+function Githubuser({name}){
+  return (
+    <div>
+      <h1>
+        {name}
+        </h1>
+      </div>
+  )
+}
+
+
+
 function Callapi() {
+  const [data,setData]= useState(null);
+  useEffect(()=>{
+    fetch(
+      `https://api.github.com/users/moonhighway`
+    )
+    .then((response)=>response.json())
+    .then (data=>setData(data));
+  },[]);
+  if (data) 
+  return (
+  <Githubuser name={data.name}/>
+  );
+  return(
+    <pre> {JSON.stringify(data,null,2)} </pre>
+  )
+
+
+
   return (
     <div>
       <h1>are you advent</h1>
