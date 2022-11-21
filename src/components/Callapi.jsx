9859fff788a745fbe42d527bb7d0dff37bd449f5
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-11-21 18:34:20 +0530

    1 file modified

--- src/components/Callapi.jsx
+++ src/components/Callapi.jsx
@@ -1,37 +1,23 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-
-function Githubuser({name}){
+function Githubuser({ name, url }) {
   return (
     <div>
-      <h1>
-        {name}
-        </h1>
-      </div>
-  )
+      <h1>{name}</h1>
+    </div>
+  );
 }
 
-
-
 function Callapi() {
-  const [data,setData]= useState(null);
-  useEffect(()=>{
-    fetch(
-      `https://api.github.com/users/moonhighway`
-    )
-    .then((response)=>response.json())
-    .then (data=>setData(data));
-  },[]);
-  if (data) 
-  return (
-  <Githubuser name={data.name}/>
-  );
-  return(
-    <pre> {JSON.stringify(data,null,2)} </pre>
-  )
-
-
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/moonhighway`)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+  if (data) return <Githubuser name={(data.name, data.url)} />;
+  return <pre> {JSON.stringify(data, null, 2)} </pre>;
 
   return (
     <div>
