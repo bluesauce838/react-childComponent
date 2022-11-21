2ae6222168a1a5f4216e529f8f0cbf12994eabe1
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-11-21 17:35:57 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const xyz = ['child', 'mind', 'component'];
+
+  // const child= xyz.map (())
+
   return (
     <div>
-    
+      <div>
+        {xyz
+          .filter((xyz) => xyz.includes('c'))
+          .map((filteredName) => (
+            <li>{filteredName}</li>
+          ))}
+      </div>
     </div>
   );
 }
